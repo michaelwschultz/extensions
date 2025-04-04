e2eb5b692ac21533f60ba7307d0e7a0653276c76
Michael Schultz <michaelwschultz@gmail.com>
2025-04-04 00:21:42 -0700

    clean up formatting

--- extensions/figma-files/src/components/AdvancedActionSection.tsx
+++ extensions/figma-files/src/components/AdvancedActionSection.tsx
@@ -3,55 +3,44 @@ import { clearFiles } from "../cache";
 import { clearVisitedFiles } from "../hooks/useVisitedFiles";
 
 export default function AdvancedActionSection(props: {
-	revalidateVisitedFiles: () => void;
-	revalidateAllFiles: () => void;
+  revalidateVisitedFiles: () => void;
+  revalidateAllFiles: () => void;
 }) {
-	const { revalidateVisitedFiles, revalidateAllFiles } = props;
-	async function handleClearCache() {
-		const toast = await showToast(Toast.Style.Animated, "Clearing cache");
+  const { revalidateVisitedFiles, revalidateAllFiles } = props;
+  async function handleClearCache() {
+    const toast = await showToast(Toast.Style.Animated, "Clearing cache");
 
-		try {
-			await clearFiles();
-			revalidateAllFiles();
-			toast.style = Toast.Style.Success;
-			toast.title = "Cleared cache";
-		} catch (error) {
-			toast.style = Toast.Style.Failure;
-			toast.title = "Failed clearing cache";
-			toast.message = error instanceof Error ? error.message : undefined;
-		}
-	}
+    try {
+      await clearFiles();
+      revalidateAllFiles();
+      toast.style = Toast.Style.Success;
+      toast.title = "Cleared cache";
+    } catch (error) {
+      toast.style = Toast.Style.Failure;
+      toast.title = "Failed clearing cache";
+      toast.message = error instanceof Error ? error.message : undefined;
+    }
+  }
 
-	async function handleClearVisited() {
-		const toast = await showToast(
-			Toast.Style.Animated,
-			"Clearing recent files",
-		);
+  async function handleClearVisited() {
+    const toast = await showToast(Toast.Style.Animated, "Clearing recent files");
 
-		try {
-			await clearVisitedFiles();
-			revalidateVisitedFiles();
-			toast.style = Toast.Style.Success;
-			toast.title = "Cleared recent files";
-		} catch (error) {
-			toast.style = Toast.Style.Failure;
-			toast.title = "Failed clearing recent files";
-			toast.message = error instanceof Error ? error.message : undefined;
-		}
-	}
+    try {
+      await clearVisitedFiles();
+      revalidateVisitedFiles();
+      toast.style = Toast.Style.Success;
+      toast.title = "Cleared recent files";
+    } catch (error) {
+      toast.style = Toast.Style.Failure;
+      toast.title = "Failed clearing recent files";
+      toast.message = error instanceof Error ? error.message : undefined;
+    }
+  }
 
-	return (
-		<ActionPanel.Section title="Advanced">
-			<Action
-				icon={Icon.DeleteDocument}
-				title="Clear Recent Files"
-				onAction={handleClearVisited}
-			/>
-			<Action
-				icon={Icon.Trash}
-				title="Clear All Cache"
-				onAction={handleClearCache}
-			/>
-		</ActionPanel.Section>
-	);
+  return (
+    <ActionPanel.Section title="Advanced">
+      <Action icon={Icon.DeleteDocument} title="Clear Recent Files" onAction={handleClearVisited} />
+      <Action icon={Icon.Trash} title="Clear All Cache" onAction={handleClearCache} />
+    </ActionPanel.Section>
+  );
 }
